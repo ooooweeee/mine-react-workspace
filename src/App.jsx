b4347b4997d0e0752041068ae00da62ab9ea3787
ooooweeee <ooooweeee@github.com>
2023-03-08 18:35:46 +0800

    删除装饰器

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React from 'react';
 
-export const PageTheme =
-  ({ theme } = {}) =>
-  WrappedComponent => {
-    return class PageNormal extends React.Component {
-      render() {
-        return (
-          <div className={['theme', theme].join(' ')}>
-            <WrappedComponent
-              {...this.props}
-              isDeep={theme === 'deep'}
-            ></WrappedComponent>
-          </div>
-        );
-      }
-    };
-  };
-
-@PageTheme({ theme: 'deep' })
 export default class App extends React.Component {
   render() {
-    return <h1>This is App - {this.props.isDeep.toString()}</h1>;
+    return <h1>This is App</h1>;
   }
 }
